f3e2d6918bce1e3cdc429df892098e77ffb705bd
Nick Ulle <nick.ulle@gmail.com>
2013-06-28 10:33:59 -0700

    Adds function to build an unpruned classification tree.

--- mlexamples/R/trees.R
+++ mlexamples/R/trees.R
@@ -1,24 +1,54 @@
 # trees.R
 # Author: Nick Ulle
 
-setClass('BinaryTreeNode')
-setClass('BinaryTreeLeaf', representation(node = 'integer'), 
-         prototype(node = NA_integer_), contains = 'BinaryTreeNode')
-setClass('BinaryTree', representation(left = 'BinaryTreeNode', 
-                                      right = 'BinaryTreeNode', 
-                                      frame = 'integer'), 
+setClass('BinaryTree')
+setClass('BinaryTreeLeaf', representation(node = 'list'), 
+         prototype(node = list()), contains = 'BinaryTree')
+setClass('BinaryTreeBranch', representation(left = 'BinaryTree', 
+                                      right = 'BinaryTree', 
+                                      node = 'list'), 
          prototype(left = new('BinaryTreeLeaf'), right = new('BinaryTreeLeaf'),
-                   node = NA_integer_), contains = 'BinaryTreeNode')
+                   node = list()), contains = 'BinaryTree')
 
+makeTree <- function(y, x, purity, minsplit) {
+    tree <- NULL # Declare tree here.
+    if (nrow(y) < minsplit) {
+        tree <- new('BinaryTreeLeaf')
+    } else {
+        tree <- new('BinaryTreeBranch')
+        tree@node <- bestCovariate(y, x, purity)
+        sub <- x[tree@node$covariate] <= tree@node$value
+        
+        y_left <- y[sub, , drop = FALSE]
+        x_left <- x[sub, ]
+        tree@left <- makeTree(y_left, x_left, purity, minsplit)
+
+        y_right <- y[!sub, , drop = FALSE]
+        x_right <- x[!sub, ]
+        tree@right <- makeTree(y_right, x_right, purity, minsplit)
+    }
+    return(tree)
+}
+
+# Finds best covariate's best split.
+bestCovariate <- function(y, x, purity) {
+    splits <- sapply(x, function(x_) {
+                           bestSplit(y, x_, purity)
+                       })
+    best <- which.min(splits[2, ])
+    list(covariate = colnames(splits)[[best]], value = splits[[1, best]])
+}
+
+# Finds best split within a covariate.
 bestSplit <- function(y, x, purity) {
     # TODO: generalize to nominal x
     xs <- unique(x)
-    xs <- xs[order(xs), ]
-    splits = sapply(xs, function(x_) {
-                            left <- y[x <= x_, ]
-                            right <- y[x > x_, ]
+    xs <- xs[order(xs)]
+    splits <- sapply(xs, function(x_) {
+                            left <- y[x <= x_, , drop = FALSE]
+                            right <- y[x > x_, , drop = FALSE]
                             err <- c(purity(left), purity(right))
-                            wgt <- c(length(left), length(right))
+                            wgt <- c(nrow(left), nrow(right))
                             c(x_, weighted.mean(err, wgt))
                         })
     splits[, which.min(splits[2, ])]
